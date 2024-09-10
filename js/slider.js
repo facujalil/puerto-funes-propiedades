611204e35270a8fc4611e830505c131582903b87
Facu J <facu.jalil@gmail.com>
2024-09-10 18:59:14 -0300

    change slider items handling

--- js/slider.js
+++ js/slider.js
@@ -1,38 +1,14 @@
 const slider = document.getElementById("slider");
-const sliderItem = document.querySelectorAll(".slider-item");
-const lastSliderItem = sliderItem[sliderItem.length - 1];
 
 const sliderLeftButton = document.querySelector(".slider-left-button");
 const sliderRightButton = document.querySelector(".slider-right-button");
 
+let sliderItems = document.querySelectorAll(".slider-item");
+let lastSliderItem = sliderItems[sliderItems.length - 1];
+
 let interval;
 
 slider.insertAdjacentElement("afterbegin", lastSliderItem);
-
-const previous = () => {
-  const sliderItem = document.querySelectorAll(".slider-item");
-  const lastSliderItem = sliderItem[sliderItem.length - 1];
-  slider.style.marginLeft = "0%";
-  slider.style.transition = "all 0.5s";
-  setTimeout(() => {
-    slider.style.transition = "none";
-    slider.insertAdjacentElement("afterbegin", lastSliderItem);
-    slider.style.marginLeft = "-100%";
-  }, 500);
-};
-
-const next = () => {
-  const firstSliderItem = document.querySelectorAll(".slider-item")[0];
-  slider.style.marginLeft = "-200%";
-  slider.style.transition = "all 0.5s";
-  setTimeout(() => {
-    slider.style.transition = "none";
-    slider.insertAdjacentElement("beforeend", firstSliderItem);
-    slider.style.marginLeft = "-100%";
-  }, 500);
-};
-
-interval = setInterval(next, 5000);
 
 sliderLeftButton.addEventListener("click", () => {
   clearInterval(interval);
@@ -45,3 +21,29 @@ sliderRightButton.addEventListener("click", () => {
   next();
   interval = setInterval(next, 5000);
 });
+
+const previous = () => {
+  sliderItems = document.querySelectorAll(".slider-item");
+  lastSliderItem = sliderItems[sliderItems.length - 1];
+  slider.style.marginLeft = "0%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(() => {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("afterbegin", lastSliderItem);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+};
+
+const next = () => {
+  sliderItems = document.querySelectorAll(".slider-item");
+  const firstSliderItem = sliderItems[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(() => {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("beforeend", firstSliderItem);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+};
+
+interval = setInterval(next, 5000);
