93482b3ab5bac9bd08d3dc6b2c8474a59f82a646
Facu J <facu.jalil@gmail.com>
2024-09-30 21:10:23 -0300

    improve form submission handling

--- js/form.js
+++ js/form.js
@@ -4,17 +4,25 @@ const submitButton = document.getElementById("submit-button");
 contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  submitButton.disabled = true;
   submitButton.textContent = "Enviando...";
 
-  emailjs.sendForm("default_service", "template_so75imr", contactForm).then(
-    () => {
-      window.location.href = "../mensaje-recibido.html";
-      submitButton.textContent = "Enviar";
-      contactForm.reset();
-    },
-    (error) => {
-      alert(JSON.stringify(error));
-      contactForm.reset();
-    }
-  );
+  emailjs
+    .sendForm(
+      "service_b0b038a",
+      "template_so75imr",
+      contactForm,
+      "CYgaeAeVZmfaobGxf"
+    )
+    .then(
+      () => {
+        window.location.href = "../mensaje-recibido.html";
+      },
+      (error) => {
+        alert(error.text);
+        submitButton.disabled = false;
+        submitButton.textContent = "Enviar";
+        contactForm.reset();
+      }
+    );
 });
